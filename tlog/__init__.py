a1b7b173b19c6c9b33d7401195ec7c4fc1fd35f6
LordKnows <zandywho@gmail.com>
2018-08-28 20:06:54 +0900

    Add layout

--- tlog/__init__.py
+++ tlog/__init__.py
@@ -1,11 +1,13 @@
 import os
 
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 
 def create_app(test_config=None):
 
     app = Flask(__name__, instance_relative_config=True)
+    Bootstrap(app)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'tlog.sqlite'),
